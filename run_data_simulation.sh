36654bf1075311a323c2879480bfbf4f1fdad2f1
TatevikJ <43089296+TatevikJ@users.noreply.github.com>
2022-06-20 23:57:10 +0400

    Update run_data_simulation.sh

--- run_data_simulation.sh
+++ run_data_simulation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #SBATCH --mem 40gb
 #SBATCH --output=slurm-%j.log
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=tatevik.jalatyan@abi.am
 
 Rscript data_simulation/data_simulation.R
